d5011678228435cc2a3e7c99ab551a2f77f1ef7e
lsb <likith15600@gmail.com>
2025-03-08 13:20:40 +0530

    Added register form

--- Frontend/intern-project/src/App.tsx
+++ Frontend/intern-project/src/App.tsx
@@ -1,10 +1,12 @@
 import Login from "./components/Login";
 import { Routes, Route } from "react-router";
+import Register from "./components/Register";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 };
